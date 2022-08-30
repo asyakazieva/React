d54770f04e1d9282d9beda69a61c2c19ccf695d3
Asya <asyakazieva@mail.ru>
2022-08-30 20:33:28 +0300

    HW3

---
+++ less3/src/components/FuncDialogue/components/Input/Input.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Input } from './Input';
+
+const changeHandler = jest.fn();
+
+describe('FuncInput', () => {
+  it('Render component', () => {
+    render(<Input value="Render test" change={changeHandler} />);
+  });
+
+  it('Render with snapshot', () => {
+    const { asFragment } = render(
+      <Input value="Test" change={changeHandler} />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('Multiple render', () => {
+    render(
+      <>
+        <Input value="Test" change={changeHandler} />
+        <Input value="Test" change={changeHandler} />
+      </>
+    );
+    expect(screen.getAllByRole('textbox').length).toBe(2);
+  });
+
+  it('Render with value and change handler', () => {
+    render(<Input value="Test" change={changeHandler} />);
+    fireEvent.input(screen.getByRole('textbox'), {
+      target: { value: 'Test' },
+    });
+    expect(screen.getByDisplayValue(/Test/)).toBeInTheDocument();
+  });
+});
